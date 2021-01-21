90c07e2404c41bbec2562bccc58f2c995a5ad8de
Bjornraaf <daysforyou1120@gmail.com>
2021-01-21 15:15:03 +0100

    Script Changes
    Mmhh

--- Assets/Scripts/Extra.cs
+++ Assets/Scripts/Extra.cs
@@ -2,49 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Extra : MonoBehaviour
+public class Extra : Spikes
 {
     private SpriteRenderer rend;
     private Sprite xSprite;
     private Sprite circleSprite;
     private Sprite triangleSprite;
+    protected Sprite circleGreenSprite;
+    //private Renderer colorRend;
+    //[SerializeField] private Color colorToTurnTo = Color.white;
 
-    // Start is called before the first frame update
-    private void Start()
+
+
+
+    protected void Start()
     {
+        //colorRend = GetComponent<Renderer>();
+        //rend.material.color = colorToTurnTo;
         rend = GetComponent<SpriteRenderer>();
         xSprite = Resources.Load<Sprite>("X");
         circleSprite = Resources.Load<Sprite>("Circle");
+        circleGreenSprite = Resources.Load<Sprite>("Circle2");
         triangleSprite = Resources.Load<Sprite>("Triangle");
         rend.sprite = xSprite;
+
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0))
         {
-            if (rend.sprite == xSprite)
-            {
-                Debug.Log("F");
-                rend.sprite = circleSprite;
-            }
-            else if (rend.sprite == circleSprite)
-            {
-                Debug.Log("U");
-                rend.sprite = triangleSprite;
-            }
-            else if (rend.sprite == triangleSprite)
-            {
-                Debug.Log("Too");
-                rend.sprite = xSprite;
-            }
+            Debug.Log("Bababooey");
+            rend.sprite = circleGreenSprite;
+
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision2D)
     {
-        if (collision2D.gameObject.tag == "Enemy") 
+        if (collision2D.gameObject.tag == "Player") //Update() + Input.GetMouseButtonDown(0)
         {
             if (rend.sprite == xSprite)
             {
---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    private SpriteRenderer rend;
+    private Sprite spikeSprite;
+    private Sprite noSpikeSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        spikeSprite = Resources.Load<Sprite>("Spike");
+        noSpikeSprite = Resources.Load<Sprite>("noSpike");
+        rend.sprite = spikeSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(rend.sprite = circleGreenSprite)
+        //{
+        //   rend.sprite = noSpikeSprite;
+        //}
+    }
+}
