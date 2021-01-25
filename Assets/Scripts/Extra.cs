cddba723b8e8d68b61a3212f06564a92a03c1f7f
Bjornraaf <daysforyou1120@gmail.com>
2021-01-26 00:33:34 +0100

    I Give Up
    Fuck Me...

--- Assets/Scripts/Extra.cs
+++ Assets/Scripts/Extra.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Extra : MonoBehaviour
 {
+    protected int points;
+
     protected SpriteRenderer rend;
-    private Sprite xSprite;
-    private Sprite circleSprite;
-    private Sprite triangleSprite;
+    protected Sprite xSprite;
+    protected Sprite circleSprite;
+    protected Sprite triangleSprite;
     protected Sprite circleGreenSprite;
     //private Renderer colorRend;
     //[SerializeField] private Color colorToTurnTo = Color.white;
@@ -31,31 +33,28 @@ public class Extra : MonoBehaviour
     // Update is called once per frame
     protected void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-           Debug.Log("Bababooey");
-           rend.sprite = circleGreenSprite;
-        
-        }
+
     }
 
-    private void OnTriggerEnter2D(Collider2D collision2D)
+    protected void OnTriggerEnter2D(Collider2D collision2D)
     {
         if (collision2D.gameObject.tag == "Player") //Update() + Input.GetMouseButtonDown(0)
         {
             if (rend.sprite == xSprite)
             {
-                Debug.Log("F");
+                points++;
+                Debug.Log(points);
                 rend.sprite = circleSprite;
             }
             else if (rend.sprite == circleSprite)
             {
-                Debug.Log("U");
+                points--;
+                Debug.Log(points);
                 rend.sprite = triangleSprite;
             }
             else if (rend.sprite == triangleSprite)
             {
-                Debug.Log("Too");
+                Debug.Log(points);
                 rend.sprite = xSprite;
             }
         }
